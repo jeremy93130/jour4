51559438c7027312ca28d56d14bbec60dbc5a2c9
jeremy Dubrulle <https://github.com/jeremy93130/jour3>
2021-10-01 12:17:30 +0200

    Fin Exercice JS

--- jour4.js
+++ jour4.js
@@ -5,7 +5,13 @@ var cat = {
     age: 3,
     isCute: true
 }
-// console.log(cat)
+
+console.log(cat.age)
+console.log(cat["age"])
+
+if (cat.isCute === true) {
+    console.log("So cute !")
+}
 
 // Exercice 2 : Combine
 
@@ -17,13 +23,17 @@ var cat2 = {
 
 var cats = [cat, cat2]
 
-// console.log(cats)
+console.log(cats)
 
-// console.log(cat.age)
-// console.log(cat["age"])
+console.log(cat.age)
+console.log(cats[0].age)
+console.log(cat["age"])
+console.log(cats[0]["age"])
 
-// console.log(cat.isCute)
-// console.log(cat["isCute"])
+console.log(cat.isCute)
+console.log(cats[1].isCute)
+console.log(cat["isCute"])
+console.log(cats[1]["isCute"])
 
 // Exercice 3 : Even 
 
@@ -40,9 +50,9 @@ checkIfEven(46)
 
 function compare(num1, num2) {
     if (num1 > num2) {
-        console.log(`${num1} is bigger`)
+        console.log(`${num1} is bigger than ${num2}`)
     } else if (num1 < num2) {
-        console.log(`${num2} is bigger`)
+        console.log(`${num2} is bigger than ${num1}`)
     } else { console.log(`${num1} & ${num2} are both the same`) }
 }
 
@@ -54,11 +64,13 @@ function addUp(num) {
     var result = 0
     for (var i = 1; i <= num; i++) {
         result += i
-        // } console.log(result)
-    }
+    } console.log(result)
+    return result
 }
 
-addUp(12)
+var sum = addUp(12)
+console.log(sum)
+
 
 
 // Exercice 6 : 
@@ -78,9 +90,37 @@ format(3700)
 
 // Bonus
 
-function generatedPassword (num) {
-    var randomString = num + (Math.random().toString(num).slice(-8)) 
-    // }console.log(randomString)
+function generatedPassword(num) {
+    var randomString = "abcdefghijklmnopqrstuvwxyz";
+    var result = "";
+    for (var i = 0; i < num; i++) {
+        result = result + randomString.charAt(Math.floor(Math.random() * randomString.length))
 
-// generatedPassword("maisouetdoncornicar")
+    } if (num < 6 || num > 15) {
+        console.log("Error")
+    } else {
+        console.log(result)
+    }
+}
+generatedPassword(10)
+
+// Bonus II 
+
+function launchDice(numberOfDice) {
+    var result = 0
+    for (var i = 0; i <= numberOfDice; i++) {
+        var de = Math.floor(Math.random() * (6 - 1 + 1) + 1)
+        result = result + de
+    } return result
+}
+var joueur1 = launchDice(5)
+var joueur2 = launchDice(5)
+console.log(joueur1, joueur2)
+
+if (joueur1 > joueur2) {
+    console.log(`Player 1 is the winner with ${joueur1}`)
+} else if (joueur2 > joueur1) {
+    console.log(`Player 2 is the winner with ${joueur2}`)
+} else if (joueur1 === joueur2) {
+    console.log(`Player 1 and Player 2 are equals with ${joueur1}`)
 }
